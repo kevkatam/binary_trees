e27d3d1121a16df9b9c9cb316965bbd2d7622679
kevkatam <kevkatam@gmail.com>
2023-11-01 07:50:09 -0700

    modified 100-binary_trees_ancestor.c

--- binary_trees.h
+++ binary_trees.h
@@ -60,7 +60,8 @@ int is_parent(const binary_tree_t *tree);
 int height(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+	        	             const binary_tree_t *second);
 size_t binary_depth(const binary_tree_t *tree);
 size_t depth(const binary_tree_t *tree);
 
