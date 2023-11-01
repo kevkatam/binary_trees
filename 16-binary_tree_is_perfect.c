f550c8d2dad5b1d1eb4f8e35319071a841851f9f
kevkatam <kevkatam@gmail.com>
2023-11-01 06:21:22 -0700

    modified 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -68,7 +68,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		if (is_leaf(tree->left) && is_leaf(tree->right))
 			return (1);
 
-		if (is_parent(tree))
+		if (is_parent(tree) == 1)
 			return (binary_tree_is_perfect(tree->left) &&
 				binary_tree_is_perfect(tree->right));
 	}
